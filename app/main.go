e43e643290bef179f56fd8627efeb6e8bbc4bad7
mucleck <franconogalesali@gmail.com>
2026-09-01 00:03:45 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -69,7 +69,7 @@ func handleType(command string) {
 		}
 
 		if strings.Contains(file.Mode().Perm().String(), "x") {
-			fmt.Println(executable + " is " + dir)
+			fmt.Println(executable + " is " + dir + executable)
 			return
 		} 
 	} 
